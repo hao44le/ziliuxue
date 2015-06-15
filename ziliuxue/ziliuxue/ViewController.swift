2a62150ec2988a8e68d02f83444d6e025841bb18
Grey Chen <geleichen@gmail.com>
2015-06-15 17:56:33 +0800

    init the project
    init

---
+++ ziliuxue/ziliuxue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ziliuxue
+//
+//  Created by Gelei Chen on 6/15/15.
+//  Copyright (c) 2015 ziliuxue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
