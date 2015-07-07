c4d573d71b801e135cbc51af7b9d43bb2fe65925
Grey Chen <geleichen@gmail.com>
2015-07-07 11:19:19 +0800

    integrate find classVC

---
+++ ziliuxue/ziliuxue/CourseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CourseTableViewCell.swift
+//  
+//
+//  Created by Gelei Chen on 7/7/15.
+//
+//
+
+import UIKit
+
+class CourseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ziliuxue/ziliuxue/FindClassViewController.swift
@@ -0,0 +1,52 @@
+//
+//  FindClassViewController.swift
+//  
+//
+//  Created by Gelei Chen on 7/7/15.
+//
+//
+
+import UIKit
+
+class FindClassViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    let name = ["TOEFL","IELTS","GRE","GMAT","LSAT"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = self.name[indexPath.row]
+        return cell
+        
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        self.performSegueWithIdentifier("toCourse", sender: self)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
