eba21b6a545d09f5961da66563895007f0f09c43
Grey Chen <geleichen@gmail.com>
2015-07-08 22:15:16 +0800

    work on school application tab VC

--- ziliuxue/ziliuxue/SchoolApplicationViewController.swift
+++ ziliuxue/ziliuxue/SchoolApplicationViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class SchoolApplicationViewController: UIViewController {
+class SchoolApplicationViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var applicationDescription: UILabel!
     @IBAction func swipeRight(sender: AnyObject) {
         let selectedIndex = self.tabBarController?.selectedIndex
         self.tabBarController?.selectedIndex = selectedIndex! - 1
@@ -19,17 +20,31 @@ class SchoolApplicationViewController: UIViewController {
         let selectedIndex = self.tabBarController?.selectedIndex
         self.tabBarController?.selectedIndex = selectedIndex! + 1
     }
+    
+    var applicationDescriptionString = "学生与教师的比例在普渡斯顿大学是6:1，学校有70.9%的班只有不到20名学生。在普林斯顿大学最受欢迎的专业包括：经济学，政治学及政府，分子生物学，心理学与公共政策分析。平均大一保留率：98.3%"
+    
+    var applicationLeft = ["基本教育","毕业课程要求","课程大学"]
+    var applicationRight = ["是","7","人类学，数学，英语，外语，理科，历史，社会科学"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.interactivePopGestureRecognizer.enabled = false
+        applicationDescription.text = applicationDescriptionString
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return applicationLeft.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("sepecificApplicationCell", forIndexPath: indexPath) as! CollegeInfoTableViewCell
+        cell.leftLabel.text = applicationLeft[indexPath.row]
+        cell.rightLabel.text = applicationRight[indexPath.row]
+        return cell
+    }
 
     /*
     // MARK: - Navigation
