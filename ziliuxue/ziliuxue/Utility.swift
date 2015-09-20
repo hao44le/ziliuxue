354fec6336c1cbe949170fe31689c69646b320d5
qianhong.ws <qianhong.ws@cainiao.com>
2015-09-20 11:01:55 +0800

    Writer Detail Page VC

--- ziliuxue/ziliuxue/Utility.swift
+++ ziliuxue/ziliuxue/Utility.swift
@@ -47,13 +47,7 @@ struct LocalStore {
     
     
 }
-struct ScreenSize
-{
-    static let SCREEN_WIDTH         = UIScreen.mainScreen().bounds.size.width
-    static let SCREEN_HEIGHT        = UIScreen.mainScreen().bounds.size.height
-    static let SCREEN_MAX_LENGTH    = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
-    static let SCREEN_MIN_LENGTH    = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
-}
+
 
 struct ServerMethods {
     
--- ziliuxue/ziliuxue/WriterDetailViewController.swift
+++ ziliuxue/ziliuxue/WriterDetailViewController.swift
@@ -13,6 +13,12 @@ class WriterDetailViewController: UIViewController{
     
     @IBOutlet var backScrollView: UIScrollView!
     
+    var teacherInfoView:ZLXTeacherInfoView!
+    var excellentField:ZLXTitleAndTagsView!
+    var probationView:ZLXFixedRowsWithTitleView!
+    var similarWriter:ZLXTitleAndTagsView!
+    var evaluationAndCommentView:ZLXTwoSegmentView!
+    
     var id:String?
     
     var detailDataDic:ServiceWriterDetail?
@@ -32,14 +38,44 @@ class WriterDetailViewController: UIViewController{
 //            NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("didGetWriterDetail:"), name: "didGetWriterDetail", object: nil)
 //        }
  
-        //self.backScrollView.contentSize = CGSizeMake(ScreenSize.SCREEN_WIDTH, 1000)
-
+        self.backScrollView.contentSize = CGSizeMake(ScreenSize.SCREEN_WIDTH, 1150)
+        self.setupShareButton()
+        self.backScrollView.backgroundColor = UIColorFromHexRGB(0xf0f4f7)
+        self.teacherInfoView = ZLXTeacherInfoView(frame: CGRectMake(5, 69, ScreenSize.SCREEN_WIDTH - 10, 280), dataModel: nil)
+        self.backScrollView.addSubview(self.teacherInfoView)
+        
+        self.excellentField = ZLXTitleAndTagsView(frame: CGRectMake(5, CGRectGetMaxY(self.teacherInfoView.frame), ScreenSize.SCREEN_WIDTH - 10, 150), array: [["excel","简历"],["excel","个人陈述"],["excel","推荐信"]])
+        self.excellentField.title.text = "擅长领域"
+        self.backScrollView.addSubview(self.excellentField)
+        
+        self.probationView = ZLXFixedRowsWithTitleView(frame: CGRectMake(5, CGRectGetMaxY(self.excellentField.frame) , ScreenSize.SCREEN_WIDTH - 10, 190), array: ["个人简历-Cleiden University","申请信-Monster University","推荐信-Hogwarts University"])
+        self.probationView.title.text = "作品试读"
+        self.backScrollView.addSubview(self.probationView)
+        
+        self.similarWriter = ZLXTitleAndTagsView(frame: CGRectMake(5, CGRectGetMaxY(self.probationView.frame) , ScreenSize.SCREEN_WIDTH - 10, 150), array: [["writer","小汪"],["writer","小猫"],["writer","小蜥蜴"],["writer","小老虎"]])
+        self.similarWriter.title.text = "相似写手"
+        self.backScrollView.addSubview(self.similarWriter)
+        
+        self.evaluationAndCommentView = ZLXTwoSegmentView(frame: CGRectMake(5, CGRectGetMaxY(self.similarWriter.frame) , ScreenSize.SCREEN_WIDTH - 10, 300))
+        self.evaluationAndCommentView.title.text = "综合评价"
+        self.backScrollView.addSubview(self.evaluationAndCommentView)
+        
   
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupShareButton(){
+        let shareButton = UIButton(frame: CGRectMake(0, 0, 44, 44))
+        shareButton.backgroundColor = UIColor.clearColor()
+        shareButton.setImage(UIImage(named: "share_button_normal"), forState: UIControlState.Normal)
+        shareButton.setImage(UIImage(named: "share_button_highlighted"), forState: UIControlState.Highlighted)
+        //shareButton.addTarget(self, action: Selector(""), forControlEvents: UIControlEvents.TouchUpInside)
+        let shareItem = UIBarButtonItem(customView: shareButton)
+        self.navigationItem.rightBarButtonItem = shareItem
     }
     
     func didGetWriterDetail(notification:NSNotification)
